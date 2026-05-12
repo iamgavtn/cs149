73c4e0ce89191c742f36f46c4418572836a0ff33
Gavin Nguyen <iamgavtn@gmail.com>
2026-05-11 18:04:07 -0700

    WS_bonus: xv6 sleep program

---
+++ WS_bonus/sleep.c
@@ -0,0 +1,19 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  int n;
+
+  if(argc != 2){
+    printf("Usage: sleep <ticks>\n");
+    exit(1);
+  }
+
+  n = atoi(argv[1]);
+  sleep(n);
+
+  exit(0);
+}
